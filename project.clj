2be6960617984b844049264fec5ddfeaf510bd51
Timothy Baldridge <tbaldridge@gmail.com>
2016-12-08 16:31:45 -0700

    typeo

--- project.clj
+++ project.clj
@@ -10,4 +10,4 @@
                    :dependencies [[com.github.javaparser/javaparser-core "2.5.1"]
                                   [org.jboss.forge.roaster/roaster-api "2.19.0.Final"]
                                   [org.jboss.forge.roaster/roaster-jdt "2.19.0.Final"]]}}
-  :plugins [[lein-release "1.0.5"]]})
+  :plugins [[lein-release "1.0.5"]])
