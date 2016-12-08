cecade5374d38454a693f711123cd9d9595267ea
Timothy Baldridge <tbaldridge@gmail.com>
2016-12-08 16:30:38 -0700

    updated project file to get ready for deployment

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject fn-fx "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A declarative wrapper for JavaFX2"
+  :url "http://github.com/halgari/fn-fx"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
@@ -9,5 +9,5 @@
   :profiles {:dev {:source-paths ["src" "examples"]
                    :dependencies [[com.github.javaparser/javaparser-core "2.5.1"]
                                   [org.jboss.forge.roaster/roaster-api "2.19.0.Final"]
-                                  [org.jboss.forge.roaster/roaster-jdt "2.19.0.Final"]]
-                   }})
+                                  [org.jboss.forge.roaster/roaster-jdt "2.19.0.Final"]]}}
+  :plugins [[lein-release "1.0.5"]]})
