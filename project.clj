f0236069f0d8dd123d58de4947f041125c1dc935
Timothy Baldridge <tbaldridge@gmail.com>
2017-08-29 08:08:27 -0600

    lein-release plugin: preparing 0.3.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject halgari/fn-fx "0.3.0-SNAPSHOT"
+(defproject halgari/fn-fx "0.3.0"
   :description "A declarative wrapper for JavaFX2"
   :url "http://github.com/halgari/fn-fx"
   :license {:name "Eclipse Public License"
