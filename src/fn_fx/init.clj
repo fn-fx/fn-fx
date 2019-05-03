50aeaa31593b257110320b66235cd40a1310c587
Răzvan Rotaru <razvan.rotaru@yes-partner.ch>
2019-05-03 15:30:09 +0300

    :ambulance: Fix platform initialisation for JavaFX 8

--- src/fn_fx/init.clj
+++ src/fn_fx/init.clj
@@ -1,9 +1,19 @@
 (ns fn-fx.init
-  (:import (javafx.application Platform)))
+  (:import (javafx.application Platform)
+           (javafx.embed.swing JFXPanel)))
 
 
 (def ^:private javafx-initialized (atom false))
 
+;; Platform/startup is available only starting with JavaFX 9
+(defmacro ^:private start-javafx []
+  (let [version (System/getProperty "javafx.runtime.version")
+        major-version (try (Integer/parseInt (re-find #"^[0-9]+" version))
+                           ; when we can't find out what JavaFX version we have, we go for 8
+                           (catch Exception e 8))]
+    (if (< major-version 9)
+      '(JFXPanel.)
+      '(Platform/startup (fn [] nil)))))
 
 
 ;; Initialize the Java FX platform
@@ -12,18 +22,17 @@
          (fn [is-initialized]
            (if-not is-initialized
              (do
-               (println "*compile-files*:" *compile-files*)
 
                (if *compile-files*
 
                  ;; AOT compilation will trigger JavaFX initialization, which starts the JavaFX application thread
                  ;; We need to make it a daemon thread, otherwise compilation will never finish
-                 (let [t (Thread. #(Platform/startup (fn [] nil)))]
+                 (let [t (Thread. #(start-javafx))]
                    (.setDaemon t true)
                    (.start t)
                    (.join t))
 
-                 (Platform/startup (fn [] nil)))
+                 (start-javafx))
                true)
              true))))
 
